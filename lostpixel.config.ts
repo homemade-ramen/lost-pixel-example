8a3437be473d876a8e0fcab958e5df67d4bbe420
kenshin <103884604+japanese-soba-noodles@users.noreply.github.com>
2023-06-15 14:36:19 +0900

    initial setting (#1)

---
+++ lostpixel.config.ts
@@ -0,0 +1,8 @@
+import type { CustomProjectConfig } from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: 'examples/storybook-build/storybook-static',
+  },
+  generateOnly: true,
+};
