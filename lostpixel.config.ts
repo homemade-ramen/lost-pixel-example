a783652e7fb9caa06107f9e9983659ac35769040
kenshin <103884604+japanese-soba-noodles@users.noreply.github.com>
2023-06-16 11:32:42 +0900

    remove options

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -4,9 +4,6 @@ export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: "./storybook-static",
   },
-  // OSS mode
-  generateOnly: true,
-  failOnDifference: true,
   lostPixelProjectId: process.env.LOST_PIXEL_PROJECT_ID,
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
